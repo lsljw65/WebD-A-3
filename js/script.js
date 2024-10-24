741da3f6ffcf2b50a23e11ce22d4e1fa01d273ac
lee <lsljw65@gmail.com>
2024-10-24 14:28:32 +0900

    슬라이더 구현

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-$(function () {});
+$(function () {
+  //   슬라이드
+  var count = 0;
+  setInterval(function () {
+    if (count === 3) {
+      count = 0;
+    }
+    $(".sliders")
+      .stop()
+      .animate({
+        left: -1200 * count,
+      });
+    count++;
+  }, 3000);
+});
